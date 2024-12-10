85e68f9f3a5452aabaa786b89d558537059cd291
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-12-10 12:22:46 +0000

    Plot Healthy Places subdomain scores

--- R/scotland health index analysis.R
+++ R/scotland health index analysis.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(geographr)
 library(healthindexscotland)
+library(ggbeeswarm)
 library(GGally)
 library(brcplot)
 library(sf)
@@ -53,4 +54,60 @@ scotland_health_index |>
   ggpairs(columns = 1:3, lower = list(continuous = "smooth"))
 
 # ---- Sub-domain scorecard for the worst-performing Council Area on social determinants (Places) ----
+# Which Council Area scores worst overall for the Healthy Places domain?
+# We'll highligh this place on the chart
+scotland_health_index |>
+  left_join(council_area_names, by = join_by(ltla24_code == ltla21_code)) |>
+  filter(healthy_places_rank == 1) |>
+  select(ltla24_code, ltla21_name)
+#--> Glasgow (S12000049)
 
+# Wrangle Healthy Places subdomains
+subdomains <-
+  scotland_health_index_subdomains |>
+  select(ltla24_code, starts_with("places_")) |>
+  pivot_longer(cols = -ltla24_code, names_to = "subdomain", values_to = "score") |>
+
+  # Prettify subdomain names
+  mutate(
+    subdomain = str_remove(subdomain, "^places_") |>
+      str_replace_all("_", " ") |>
+      str_to_sentence()
+  ) |>
+  mutate(subdomain = fct_rev(subdomain)) |>
+
+  left_join(council_area_names, by = join_by(ltla24_code == ltla21_code))
+
+subdomains |>
+  ggplot(aes(x = score, y = subdomain)) +
+
+  geom_vline(xintercept = 0, linetype = 2) +  # Show line for average
+  geom_beeswarm(aes(text = ltla21_name)) +
+
+  scale_x_continuous(
+    breaks = c(-2.7, 0, 1.4),
+    labels = c("← Worse than average", "Average", "Better than average →"),
+    position = "top"
+  ) +
+
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 16, face = "bold", color = brc_colours$black_shadow),
+    plot.title.position = "plot",
+    panel.background = element_rect(fill = "white", colour = NA),
+    plot.background = element_rect(fill = "white", colour = NA),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  ) +
+
+  labs(
+    title = "Scores for each social determinant of health",
+    x = NULL,
+    y = NULL
+  )
+
+ggsave("outputs/scotland-healthy-places-subdomains.png", width = 200, height = 100, units = "mm")
+
+# Use interactive plot to manually check which Council Areas rank where
+library(plotly)
+ggplotly()
